353584be3d5c24b79555770fa2c45ad040dab388
Henrikas <matusevicius.henrikas@gmail.com>
2019-02-22 14:38:38 +0000

    adding the files

---
+++ Lab_13/ram.vhd
@@ -0,0 +1,50 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity ram is
+	port
+	(
+		data	: in std_logic_vector(7 downto 0);
+		addr	: in natural range 0 to 3;
+		we		: in std_logic;
+		clk		: in std_logic;
+		q		: out std_logic_vector(7 downto 0)
+	);
+	
+end entity;
+
+architecture rtl of ram is
+
+	-- Build a 2-D array type for the RAM
+	subtype word_t is std_logic_vector(7 downto 0);
+	type memory_t is array(3 downto 0) of word_t;
+	
+	-- Declare the RAM signal.
+	signal ram : memory_t;
+	
+	-- Register to hold the address
+	signal addr_reg : natural range 0 to 3;
+	
+	-- Buffer to hold the data
+	signal input_buffer : std_logic_vector(7 downto 0);
+
+begin
+
+	process(clk)
+	begin
+		if(rising_edge(clk)) then
+			if(we = '1') then
+				ram(addr) <= input_buffer;
+				input_buffer <= data;
+			end if;
+			
+			-- Register the address for reading
+			addr_reg <= addr;
+		end if;
+	
+	end process;
+	
+	q <= ram(addr_reg);
+	
+end rtl;
---
+++ Lab_13/tb_multiplier.vhd
@@ -0,0 +1,94 @@
+----------------------------------------------------------------------------------
+-- Module Name: fsm_counter - Testbench
+-- Project Name: Loughborough University - 18WSB020 Introduction to FPGA Design
+-- Target Devices: ZedBoard - Zynq 7000 (xc7z020clg484pkg)
+-- Tool Versions: Vivado 2016.4
+-- Description: Simple finite state machine counter testbench file.
+--
+-- Dependencies: None
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+----------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+-- uncomment the following library declaration if using
+-- arithmetic functions with signed or unsigned values
+--use ieee.numeric_std.all;
+
+entity tb_multiplier is
+        end tb_multiplier;
+
+architecture behavior of tb_multiplier is
+
+        -- component declaration for the unit under test (uut)
+
+        component ram
+                port ( 
+                    data : in std_logic_vector(7 downto 0);
+                    addr : in natural range 0 to 3;
+                    we   : in std_logic;
+                    clk  : in std_logic;
+                    q    : out std_logic_vector(7 downto 0)
+                );
+        end component;
+
+        --inputs
+        signal input : std_logic_vector(7 downto 0) := "00000000";
+        signal addr  : natural range 0 to 3 := 0;
+        signal we    : std_logic := '0';
+        signal clk   : std_logic;
+
+        --Outputs
+        signal q     : std_logic_vector(7 downto 0);        
+
+        -- clock period definitions
+        constant clk_period : time := 10 ns;
+
+begin
+
+        -- instantiate the unit under test (uut)
+        uut: ram port map (
+            data => input,
+            addr => addr,
+            we   => we,
+            clk  => clk,
+            q    => q
+        );
+
+        -- clock process definitions
+        clk_process :process
+        begin
+            clk <= '0';
+            wait for clk_period/2;
+            clk <= '1';
+            wait for clk_period/2;
+        end process;
+
+
+        -- stimulus process
+        stim_proc: process
+        begin
+            we    <= '1';
+            input <= "00000001";
+            addr  <= 0;
+            wait for 10 ns;
+            we    <= '0';
+            input <= "00000011";
+            addr  <= 1;
+            wait for 10 ns;
+            we    <= '1';
+            input <= "00000111";
+            addr  <= 2;    
+            wait for 10 ns;        
+            we    <= '0';
+            input <= "00001111";
+            addr  <= 3;            
+        wait;
+        end process;
+
+end;
