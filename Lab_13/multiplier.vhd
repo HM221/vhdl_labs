8fdeb3841477f6e81244639241634a10ac16ced3
Henrikas <matusevicius.henrikas@gmail.com>
2019-02-22 14:34:24 +0000

    Reordered files

--- Lab_13/multiplier.vhd
+++ Lab_13/multiplier.vhd
@@ -17,5 +17,28 @@ architecture behav of multiplier is
 	component pipeline is
 		port
 		(
-			clk : in std_logic
-		)
+			clk : in std_logic;
+			data_in : in std_logic_vector(7 downto 0);
+			data_out : out std_logic_vector(7 downto 0);
+			a : in std_logic_vector(7 downto 0);
+			b : in std_logic_vector(7 downto 0);
+			c : in std_logic_vector(7 downto 0)
+		);
+	end component;
+	
+	component ram is
+	   port
+	   (
+	       signal clk : in std_logic;
+	       signal di  : in std_logic_vector(7 downto 0);
+	       signal a   : in std_logic_vector(4 downto 0);
+	       signal we  : in std_logic;
+	       signal do  : out std_logic_vector(7 downto 0)
+	   );
+    end component;
+
+begin
+
+    
+
+end behav;
